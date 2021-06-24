c3c520bb46fa494242c10b1c45a7169d4f865948
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-06-24 11:09:38 +0530

    Create HollowInvertedMirroredRightTriangle.java

---
+++ HollowInvertedMirroredRightTriangle.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class HollowInvertedMirroredRightTriangle
+{
+ 
+public static void main(String[] args)
+{
+ 
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter N : ");
+int n=sc.nextInt(); 
+System.out.print("Enter Symbol : ");
+ 
+char c = sc.next().charAt(0);
+ 
+for(int i=n;i>0;i--)
+{
+for(int j=n-i;j>0;j--)
+ 
+{
+System.out.print(" ");
+}
+if(i==1 || i==n)
+for(int j=1;j<=i;j++)
+ 
+{
+System.out.print(c);
+}
+else
+{
+for(int j=1;j<=i;j++)
+ 
+{ 
+if(j==1 || j==i)
+System.out.print(c);
+else
+ 
+System.out.print(" ");
+}
+}
+System.out.println();
+ 
+}
+ 
+ 
+}
+}

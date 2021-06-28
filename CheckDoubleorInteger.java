ad5d6fabc8b8040b6a230ad99dee86db11463d56
Naveen <70085321+naveen701526@users.noreply.github.com>
2021-06-28 12:15:23 +0530

    Create CheckDoubleorInteger.java

---
+++ CheckDoubleorInteger.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CheckDoubleorInteger {
+ public static void main(String[] args) {
+  double d_num = 5.44444;
+  if ((d_num % 1) == 0) {
+   System.out.println("It's not a double number");
+  } else {
+   System.out.println("It's a double number");
+  }
+ }
+}
